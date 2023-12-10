2d8cf9fba04dce9bcfe354d6347ee12da53e290b
Esraa <esraa@localhost.localdomain>
2023-12-10 19:37:09 +0200

    create Db function

--- dbfunctions.sh
+++ dbfunctions.sh
@@ -1,3 +1,39 @@
 #!/bin/bash
 
-if []
+shopt -s extglob
+
+function directoryExists() {
+    if [ -d "$1" ]; then
+        return 0  
+    else
+        return 1  
+    fi
+}
+
+read -p "Enter database name: " dirName
+
+if [[ ! "$dirName" =~ [a-zA-Z0-9] ]]; then
+    echo "Invalid name."
+else
+    if directoryExists "$dirName"; then
+        echo "Database already exists."
+    else
+        mkdir "$dirName"
+        echo "Database '$dirName' created successfully."
+    fi
+fi
+
+function createDb() {
+    read -p "Enter database name: " dirName
+
+    if [[ "$dirName" =~ [a-zA-Z0-9] ]]; then
+        echo "Invalid name."
+    else
+        if directoryExists "$dirName"; then
+            echo "Database already exists."
+        else
+            mkdir "$dirName"
+            echo "Database '$dirName' created successfully."
+        fi
+    fi
+}
